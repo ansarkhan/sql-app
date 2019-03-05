e28de336c5e810210de90be546f41640fde2954f
ansarkhan <ansarkhan@me.com>
2019-03-05 17:28:43 -0500

    update working

--- bamazon.js
+++ bamazon.js
@@ -9,12 +9,12 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log(`connected as ID ${connection.threadId} \n`);
-});
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     // console.log(`connected as ID ${connection.threadId} \n`);
+// });
 
-var readProducts = function() {
+var readAllProducts = function() {
     console.log("Printing all products.... \n");
     connection.query("SELECT * FROM  products", function(err,res) {
         if (err) throw err;
@@ -22,6 +22,59 @@ var readProducts = function() {
         connection.end();
 
     });
+};
+
+var findProduct = function(item, quantity) {
+    connection.query({
+        sql: `SELECT * FROM products WHERE product_name = ?`,
+        values: [item]
+      }, function (error, results, fields) {
+        if (error) throw error;
+        if (results[0].stock_quantity < quantity) {
+            console.log(`Sorry, we don't have that many of ${item}`);
+        } else {
+            var remaining = results[0].stock_quantity - quantity;
+            updateInventory(item, remaining);
+        }
+        // console.log(results[0].stock_quantity);
+        connection.end();
+      });
+};
+
+var updateInventory = function(item, quantity) {
+    connection.query({
+        sql: `UPDATE products SET ? WHERE ?`,
+        values: [
+        {stock_quantity: quantity},
+        {product_name: item}
+        ]
+    }, function(error, results, fields) {
+        console.log("Quantity updated!");
+    
+    });
+};
+
+var openStore = function() {
+    inquirer.prompt([
+        {
+            name: "buy",
+            message: "What would you like to buy"
+        },
+        {
+            name: "quantity",
+            message: "How many units would you like?"
+        }
+    ]).then(function(answer) {
+        console.log(answer);
+        var item = answer.buy;
+        var quantity = answer.quantity;
+
+        //finds product and updates inventory if quantity is there
+        findProduct(item, quantity);
+
+
+    });
 }
 
-readProducts(); 
+openStore();
+
